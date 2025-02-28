f3d61bfe8cac49c8923e101a9e810e777d976e71
danDrilea <danalexandrudrilea@gmail.com>
2025-02-28 20:08:59 +0200

    Activitate seminar 01 - implementare strucutura dupa sablon

--- Drilea_Dan-Alexandru_ActivitateSD2025/Seminar01.c
+++ Drilea_Dan-Alexandru_ActivitateSD2025/Seminar01.c
@@ -1,35 +1,50 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-struct Sablon {
+struct Masina
+{
 	int id;
-	//int ;
-	//char* ;
-	//float ;
-	//char ;
+	int nrKm;
+	char* model;
+	float pret;
+	char tipMotor;
 };
 
-struct Sablon initializare(/*parametri necesari*/) {
-	struct Sablon s;
-	s.id = 1;
-	//initializare structura 
-	return s;
+struct Masina initializare(int id, int nrKm, const char* model, float pret, char tipMotor)
+{
+	struct Masina m;
+	m.id = id;
+	m.nrKm = nrKm;
+	m.pret = pret;
+	m.tipMotor = tipMotor;
+	m.model = (const char*)malloc(strlen(model) * sizeof(const char) + 1);
+	strcpy(m.model, model);
+	return m;
 }
 
-void afisare(struct Sablon s) {
-	//afisarea tuturor atributelor.
+void afisare(struct Masina m)
+{
+	printf("ID: %d\nNrKm: %d\nModel: %s\nPret: %.2f\nTipMotor: %c\n\n", m.id, m.nrKm, m.model, m.pret, m.tipMotor);
 }
 
-void modifica_Atribut(struct Sablon s) {
-	//modificarea unui atribut
+void modifica_Pret(struct Masina* m, float pretNou)
+{
+	if (pretNou > 0);
+	(*m).pret = pretNou;
 }
 
-void dezalocare(struct sablon* s) {
-	//dezalocare campuri alocate dinamic
+void dezalocare(struct Masina* m) {
+	free((*m).model);
+	(*m).model = NULL;
 }
 
 int main() {
-	struct Sablon s;
+	struct Masina masina = initializare(1, 100, "Logan", 4000.50, 'B'); // 'B'
+	afisare(masina);
+	modifica_Pret(&masina, 3000.50);
+	afisare(masina);
 
+	dezalocare(&masina);
+	afisare(masina);
 	return 0;
 }
